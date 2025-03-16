ba845bfd4e06908c706bbee26ff9747f90594bc1
jannatcodingal <jannatrehman007@gmail.com>
2025-03-16 19:14:30 +0500

    elif

---
+++ elif statements.py
@@ -0,0 +1,14 @@
+a=10
+b=12
+c=0
+if a and b and c:
+    print("all the statements have boolean value as true")
+else:
+    print("atleast one number has boolean value as False")
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("either of them is greater than 0")
+else:
+    print("No number is greater than 0")
